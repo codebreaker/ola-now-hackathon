52354262bbb4c90472ed31f4151be435ac8ce8f1
mahes <maheswaranapk@gmail.com>
2015-09-27 14:52:56 +0530

    Updated ReadMe

---
+++ android/Readme.md
@@ -0,0 +1,19 @@
+# **OLA-Now**
+A Powerful Cab Assistant.
+
+This Application make use of User Data in their Phones like Calendar Events, SMS, etc... to indentify their need to Book a Cab with Custom App Booking Reminders.
+
+Features[In Android]:
+Listing out Situtation where User might Need a Cab.
+Get Events From Local Calendar.
+Get Details From SMS [Partially Completed]
+Custom Reminder which Reminds User to Book Cab.
+Demand User To Book Cab to Go home at Evening & Go Office at Morning.[Partially Completed]
+Setting Screen [Partially Completed]
+
+Features[In iOS]:
+Listing out Situtation where User might Need a Cab.
+Get Events From Local Calendar.
+Setting Screen [Partially Completed]
+
+[Android Screenshots]
--- android/app/src/main/java/com/scriptedpapers/olanow/utils/ReminderReceiver.java
+++ android/app/src/main/java/com/scriptedpapers/olanow/utils/ReminderReceiver.java
@@ -3,12 +3,17 @@ package com.scriptedpapers.olanow.utils;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.PowerManager;
+import android.support.v4.app.NotificationCompat;
 import android.view.WindowManager;
 import android.widget.Toast;
+
+import com.scriptedpapers.olanow.MainActivity;
+import com.scriptedpapers.olanow.R;
 
 /*
  * Receiver Class to Receive Reminder Pending intent
@@ -28,7 +33,17 @@ public class ReminderReceiver extends BroadcastReceiver {
 				Toast.makeText(mContext, "Reminder Called", Toast.LENGTH_SHORT)
 						.show();
 
-				
+			NotificationCompat.Builder mBuilder =   new NotificationCompat.Builder(mContext)
+					.setSmallIcon(R.drawable.ola_logo) // notification icon
+					.setContentTitle("OLA Reminder!") // title for notification
+					.setContentText("You have a Reminder to Book a Cab Now.") // message for notification
+					.setAutoCancel(true); // clear notification after click
+			Intent intent = new Intent(mContext, MainActivity.class);
+			PendingIntent pi = PendingIntent.getActivity(mContext,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+			mBuilder.setContentIntent(pi);
+			NotificationManager mNotificationManager =
+					(NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+			mNotificationManager.notify(0, mBuilder.build());
 
 
 

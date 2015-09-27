fd66c5b8f88e80b0383bb05b2c1cf571abe2e6bd
mahes <maheswaranapk@gmail.com>
2015-09-27 12:10:06 +0530

    Reminder Class Updated

--- android/app/src/main/java/com/scriptedpapers/olanow/data/Reminder.java
+++ android/app/src/main/java/com/scriptedpapers/olanow/data/Reminder.java
@@ -19,9 +19,6 @@ public class Reminder {
         return reminderName;
     }
 
-    public Date getReminderDate() {
-        return new Date(reminderDate);
-    }
 
     public void setReminderId(String reminderId) {
         this.reminderId = reminderId;
@@ -33,5 +30,14 @@ public class Reminder {
 
     public void setReminderDate(Date reminderDate) {
         this.reminderDate = reminderDate.getTime();
+    }
+
+    public void setReminderDate(long reminderDate) {
+        this.reminderDate = reminderDate;
+    }
+
+
+    public long getReminderDate() {
+        return reminderDate;
     }
 }
